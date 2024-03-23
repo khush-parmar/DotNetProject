d877b92fe55beea72e077202e50b4ff47f8b4a30
khushal 2 <khushalparmar66675@gmail.com>
2024-03-23 22:21:56 +0530

    I added files

---
+++ abc/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace abc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtnm = new System.Windows.Forms.TextBox();
+            this.txtct = new System.Windows.Forms.TextBox();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btndel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnselect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "city";
+            // 
+            // txtnm
+            // 
+            this.txtnm.Location = new System.Drawing.Point(119, 61);
+            this.txtnm.Name = "txtnm";
+            this.txtnm.Size = new System.Drawing.Size(100, 22);
+            this.txtnm.TabIndex = 2;
+            // 
+            // txtct
+            // 
+            this.txtct.Location = new System.Drawing.Point(119, 127);
+            this.txtct.Name = "txtct";
+            this.txtct.Size = new System.Drawing.Size(100, 22);
+            this.txtct.TabIndex = 3;
+            // 
+            // btnsave
+            // 
+            this.btnsave.Location = new System.Drawing.Point(49, 213);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(75, 23);
+            this.btnsave.TabIndex = 4;
+            this.btnsave.Text = "save";
+            this.btnsave.UseVisualStyleBackColor = true;
+            this.btnsave.Click += new System.EventHandler(this.btnsave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 284);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(987, 229);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // btndel
+            // 
+            this.btndel.Location = new System.Drawing.Point(177, 213);
+            this.btndel.Name = "btndel";
+            this.btndel.Size = new System.Drawing.Size(75, 23);
+            this.btndel.TabIndex = 6;
+            this.btndel.Text = "delete";
+            this.btndel.UseVisualStyleBackColor = true;
+            this.btndel.Click += new System.EventHandler(this.btndel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "id";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(119, 13);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(100, 22);
+            this.txtid.TabIndex = 8;
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(319, 212);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnupdate.TabIndex = 9;
+            this.btnupdate.Text = "update";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btnselect
+            // 
+            this.btnselect.Location = new System.Drawing.Point(464, 212);
+            this.btnselect.Name = "btnselect";
+            this.btnselect.Size = new System.Drawing.Size(75, 23);
+            this.btnselect.TabIndex = 10;
+            this.btnselect.Text = "select";
+            this.btnselect.UseVisualStyleBackColor = true;
+            this.btnselect.Click += new System.EventHandler(this.btnselect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1043, 525);
+            this.Controls.Add(this.btnselect);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btndel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.txtct);
+            this.Controls.Add(this.txtnm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtnm;
+        private System.Windows.Forms.TextBox txtct;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btndel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btnselect;
+    }
+}
+
---
+++ abc/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace abc
+{
+    public partial class Form1 : Form
+    {
+        string constr = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=D:\khushal\abc\abc\vikram.mdf;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bindgrid();
+        }
+
+        private void bindgrid()
+        {
+            SqlConnection cn = new SqlConnection(constr);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select * from mytbl", cn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            cn.Close();
+          
+        }
+        private void clearall()
+        {
+            this.txtid.Clear();
+            this.txtnm.Clear();
+            this.txtct.Clear();
+        }
+        private void btnsave_Click(object sender, EventArgs e)
+        {
+            savedata();
+            clearall();
+            bindgrid();
+        }
+        private void savedata()
+        {
+            SqlConnection cn = new SqlConnection(constr);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("insert into mytbl(id,name,city)values(@id,@nm,@ct)", cn);
+            cmd.Parameters.AddWithValue("@id", txtid.Text);
+            cmd.Parameters.AddWithValue("@nm", txtnm.Text);
+            cmd.Parameters.AddWithValue("@ct", txtct.Text);
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("data inserted");
+        }
+
+        private void btndel_Click(object sender, EventArgs e)
+        {
+            deletedata();
+            bindgrid();
+            clearall();
+        }
+
+        private void deletedata()
+        {
+            SqlConnection cn = new SqlConnection(constr);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("delete from mytbl where id=@id", cn);
+            cmd.Parameters.AddWithValue("@id", txtid.Text);
+            cmd.ExecuteNonQuery();
+            cn.Close();
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            Updatedata();
+            bindgrid();
+            clearall();
+        }
+
+        private void Updatedata()
+        {
+            SqlConnection cn = new SqlConnection(constr);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("update mytbl set name=@nm,city=@ct where id=@id", cn);
+            cmd.Parameters.AddWithValue("@id", txtid.Text);
+            cmd.Parameters.AddWithValue("@nm", txtnm.Text);
+            cmd.Parameters.AddWithValue("@ct", txtct.Text);
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("data updated");
+        }
+
+        private void btnselect_Click(object sender, EventArgs e)
+        {
+            Selectdata();
+        }
+
+        private void Selectdata()
+        {
+            SqlConnection cn = new SqlConnection(constr);
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select * from mytbl where id=@id", cn);
+            cmd.Parameters.AddWithValue("@id",txtid.Text);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            txtnm.Text = dt.Rows[0]["name"].ToString();
+            txtct.Text = dt.Rows[0]["city"].ToString();
+            cn.Close();
+        }
+
+       
+
+        
+    }
+}
